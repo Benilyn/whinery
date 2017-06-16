1ae1090f6e4f655b084f2653d7a34b21a2a48389
Benilyn <benilyn.gonzales@gmail.com>
2017-06-15 22:22:51 -0700

    added const PORT

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express	=	require('express');
 const app		=	express();
 
 const {Restaurant, Comment} = require('./models');
-
+const {PORT} = require('./config');
 
 app.use(express.static('public'));
-app.listen(process.env.PORT||8080);
 
 module.exports	=	app;
