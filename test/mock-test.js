0f51100934840271b3b7b5be68804ec4fd33e110
Benilyn <benilyn.gonzales@gmail.com>
2017-06-15 23:53:16 -0700

    create test for GET endpoint

--- test/mock-test.js
+++ test/mock-test.js
@@ -6,5 +6,21 @@ const app		= require('../server.js');
 const {Restaurant} = require('../models.js');
 const {mockData} = require('./mock-data.js');
 
+const {PORT}	= require('../config.js');
+
 
 chai.use(chaiHttp);
+
+describe('GET endpoint', function() {
+	it('should return all existing restaurants', function() {
+		let res;
+		return chai.request(app)
+			.get('/restaurants')
+			.then(function(_res) {
+				res = _res;
+				res.should.have.status(200);
+				res.body.restaurants.should.have.length.of.at.least(1);
+			}); //.then function
+	}); //it(should return all existing restaurants)
+
+}); //describe GET endpoint
