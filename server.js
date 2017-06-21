5d80a847a85c57afee3930832acff9a36a64ba46
Benilyn <benilyn.gonzales@gmail.com>
2017-06-20 21:33:40 -0700

    add app.get

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const {PORT} = require('./config');
 
 app.use(express.static('public'));
 
+app.get('/data', (req, res) => {
+	res.json({name: "ABC"});
+}); //app.get(/data)
+
 module.exports	=	app;
