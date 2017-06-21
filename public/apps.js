e69def03b3b5541c93a8bed0593823622367d01d
Benilyn <benilyn.gonzales@gmail.com>
2017-06-20 21:35:08 -0700

    added and display mock data

--- public/apps.js
+++ public/apps.js
@@ -1,60 +1,6 @@
-/* jshint esversion: 6 */
-
-const mockData = {
-	
-	"restaurants": [
-		{
-			name: "restaurant1",
-			cuisine: "Italian",
-			address: "123 ABC St.",
-			city: "Los Angeles",
-			state: "CA",
-			zip: "12345"
-		},
-		{
-			name: "restaurant2",
-			cuisine: "Chinese",
-			address: "456 ABC St.",
-			city: "Los Angeles",
-			state: "CA",
-			zip: "67890"
-		},
-		{
-			name: "restaurant3",
-			cuisine: "Mexican",
-			address: "987 XYZ St.",
-			city: "Anaheim",
-			state: "CA",
-			zip: "99999"
-		},
-		{
-			name: "restaurant4",
-			cuisine: "French",
-			address: "246 ASD St.",
-			city: "Yorba Linda",
-			state: "CA",
-			zip: "35791"
-		},{
-			name: "restaurant5",
-			cuisine: "Japanese",
-			address: "876 JKL St.",
-			city: "Los Angeles",
-			state: "CA",
-			zip: "12345"
-		}
-	] //restaurants
-}; //const mockData
-
-function displayMockData(data) {
-	for (var i in data.restaurants) {
-		$('body').append('<p>' + data.restaurants[i] + '</p>');
-	}
-} //displayMockData function
-
 $(document).ready(function() {
-	event.preventDefault();
-	displayMockData();
-});
-
-
-
+	$.getJSON('/data')
+		.then(function(res){
+			console.log(res);
+		});
+}); //document.ready
