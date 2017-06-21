8bb7ad6fe823d0e2cbaf2d78bf23f4de3705b5f6
Benilyn <benilyn.gonzales@gmail.com>
2017-06-20 22:55:34 -0700

    added const mockData, app.listen and changed parameter for app.get{res.json} to reflect mockData

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app		=	express();
 
 const {Restaurant, Comment} = require('./models');
 const {PORT} = require('./config');
+const mockData = require('./mock-data');
 
 app.use(express.static('public'));
 
+app.listen(process.env.PORT||8080);
+
 app.get('/data', (req, res) => {
-	res.json({name: "ABC"});
+	res.json({mockData});
 }); //app.get(/data)
 
 module.exports	=	app;
