805b1f0e6ff6e073d7441f3ac7b20c65a7b41ade
Benilyn <benilyn.gonzales@gmail.com>
2017-06-22 07:35:01 -0700

    changed re.json({mockData}) to res.json(mockData.restaurants) and added app.use for restaurantsRouter

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express	=	require('express');
 const app		=	express();
 
+const restaurantsRouter = require('./restaurantsRouter');
+
 const {Restaurant, Comment} = require('./models');
 const {PORT} = require('./config');
 const mockData = require('./mock-data');
@@ -10,7 +12,9 @@ app.use(express.static('public'));
 app.listen(process.env.PORT||8080);
 
 app.get('/data', (req, res) => {
-	res.json({mockData});
+	res.json(mockData.restaurants);
 }); //app.get(/data)
+
+app.use('/restaurants', restaurantsRouter);
 
 module.exports	=	app;
