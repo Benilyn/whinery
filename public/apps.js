37b3fb3113541f7209d616d5bb14dd232989739b
Benilyn <benilyn.gonzales@gmail.com>
2017-06-22 07:37:30 -0700

    added code to display restaurants

--- public/apps.js
+++ public/apps.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
 	$.getJSON('/data')
 		.then(function(res){
-			$('body').append('<p>' + JSON.stringify(res) + '</p>');
-			console.log(JSON.stringify(res));
+			res.forEach(displayRestaurant);
+			
+			
 		});
+
 }); //document.ready
+
+
+function displayRestaurant(restaurant) {
+/*	$('.restaurants').append(
+		'<a href="restaurantDetails.html">' +
+		'<div><p>' + restaurant.name + '</p></div>' +
+		'</a>');
+*/	console.log(restaurant);
+	$('.restaurants').append(
+		`<a href="restaurantDetails.html">
+			<div>
+				<ul>
+					<li class='name'> ${restaurant.name} </li>
+					<span class='address'> ${restaurant.address}</span><br>
+						<span class='city'> ${restaurant.city}</span>
+						<span class='state'> ${restaurant.state}</span>
+						<span class='zip'> ${restaurant.zip}</span><br>
+				</ul>
+			</div>
+		</a>`);
+}
+
+
