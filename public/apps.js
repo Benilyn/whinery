8ace49c72dcff3e968f42096d105a5596c0bd7f8
Benilyn <benilyn.gonzales@gmail.com>
2017-06-21 21:51:25 -0700

    added code to show data on frontend

--- public/apps.js
+++ public/apps.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$.getJSON('/data')
 		.then(function(res){
-			console.log(res);
+			$('body').append('<p>' + JSON.stringify(res) + '</p>');
+			console.log(JSON.stringify(res));
 		});
 }); //document.ready
