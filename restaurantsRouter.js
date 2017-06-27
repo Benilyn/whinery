2ed1d4813f13acf8d29eb539380c047d2187c180
Benilyn <benilyn.gonzales@gmail.com>
2017-06-27 07:36:52 -0700

    create and GET endpoint

--- restaurantsRouter.js
+++ restaurantsRouter.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
-const {Restaurants} = require('./models');
 
+const {Restaurant} = require('./models');
+const {PORT, DATABASE_URL} = require('./config');
+/*
 router.get('/', (req, res) => {
-	res.json(Restaurants.get());
+	res.json(Restaurant.get());
 }); //router.get
+*/
+router.get('/', (req, res) => {
+	console.log('testing if it works');
+	Restaurant
+		.find()
+		.limit(10)
+		.exec()
+		.then(restaurants => {
+			res.json(
+				restaurants.map(
+					(restaurant) => restaurant.apiRepr())
+			);
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: "Internal server error"});
+		});
+});
 
 module.exports = router;
