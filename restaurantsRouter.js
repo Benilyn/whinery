e45c49a2bccba8f9bb40a3bbc24c7f0bf0d4f230
Benilyn <benilyn.gonzales@gmail.com>
2017-06-27 07:43:54 -0700

    create POST endpoint

--- restaurantsRouter.js
+++ restaurantsRouter.js
@@ -10,7 +10,6 @@ router.get('/', (req, res) => {
 }); //router.get
 */
 router.get('/', (req, res) => {
-	console.log('testing if it works');
 	Restaurant
 		.find()
 		.limit(10)
@@ -25,6 +24,41 @@ router.get('/', (req, res) => {
 			console.error(err);
 			res.status(500).json({message: "Internal server error"});
 		});
-});
+}); //router.get
+
+
+router.post('/', (req, res) => {
+	const requiredFields = [
+		'name',
+		'cuisine',
+		'address',
+		'city',
+		'state'
+	]; //requiredFields
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+		return res.status(400).send(message);
+		} //if (!(field in req.body))
+	} //for (let i=0)
+
+	Restaurant
+		.create({
+			name: req.body.name,
+			cuisine: req.body.cuisine,
+			address: req.body.address,
+			city: req.body.city,
+			state: req.body.state,
+			zipcode: req.body.zipcode})
+		.then(
+			restaurant => res.status(201).json(restaurant.apiRepr()))
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal server error'});
+		});
+}); //router.post
+
 
 module.exports = router;
