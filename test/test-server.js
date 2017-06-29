2f52ca8da5d41dcf86ecc10af857da7ce9c5450b
Benilyn <benilyn.gonzales@gmail.com>
2017-06-29 02:48:07 -0700

    create test for PUT endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -45,7 +45,6 @@ describe('Restaurant on GET endpoint', function() {
 }); //describe(Restaurant on GET point)
 
 describe('Restaurant on POST endpoint', function() {
-
 	it('should add a new restaurant', function() {
 		const newRestaurant = {
 			name: "Testing Restaurant Name",
@@ -71,3 +70,52 @@ describe('Restaurant on POST endpoint', function() {
 			}); //.then function
 	}); //it(should add a new restaurant)
 }); //describe(Restaurant on POST endpoint)
+
+describe('Restaurant on DELETE endpoint', function() {
+	it('should delete restaurant on DELETE', function() {
+		return chai.request(app)
+			.get('/restaurants')
+			.then(function(res) {
+				return chai.request(app)
+				.delete(`/restaurants/${res.body[0].id}`);
+			}) //.then function .delete
+			.then(function(res) {
+				res.should.have.status(204);
+			}); //.then function status(204)
+	}); //it(should delete restaurant on delete)
+}); //describe(Restaurant on DELETE endpoint)
+
+describe('Restaurant on PUT endpoint', function() {
+	it('should update restaurant on PUT', function() {
+		const updateRestaurant = {
+			name: "Testing PUT",
+			cuisine: "Asian",
+			address: "890 Test Drive",
+			city: "Wala Wala",
+			state: "CA"
+		}; //const updateRestaurant
+
+		return chai.request(app)
+			.get('/restaurants')
+			.then(function(res) {
+				updateRestaurant.id = res.body[0].id;
+				return chai.request(app)
+					.put(`/restaurants/${updateRestaurant.id}`)
+					.send(updateRestaurant)
+					.then(function(res) {
+						res.should.have.status(201);
+						res.should.be.json;
+						res.body.should.be.a('object');
+						res.body.should.deep.equal(Object.assign(updateRestaurant));
+					}); //.then function (status 201)
+			}); //.then function (updateRestaurant)
+	}); //it(should dupdate restaurant on PUT)
+}); //describe (Restaurant on PUT endpoint)
+
+
+
+
+
+
+
+
