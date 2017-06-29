b285189b9a40f46766ab42fee7f8fb5e8a1978cc
Benilyn <benilyn.gonzales@gmail.com>
2017-06-29 02:08:01 -0700

    added exports.TEST_DATABASE_URL

--- config.js
+++ config.js
@@ -3,3 +3,6 @@ exports.PORT = process.env.PORT || 8080;
 exports.DATABASE_URL =	process.env.DATABASE_URL ||
 						global.DATABASE_URL ||
 						'mongodb://localhost/whinery';
+
+exports.TEST_DATABASE_URL =	(process.env.TEST_DATABASE_URL ||
+						'mongodb://localhost/test-whinery');
