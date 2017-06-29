2194cf8bc90354ee5d91620b398fdf57a6bb9e61
Benilyn <benilyn.gonzales@gmail.com>
2017-06-29 02:11:26 -0700

    create test for GET endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -7,14 +7,39 @@ const app		= require('../server.js');
 
 chai.use(chaiHttp);
 
-describe('Index.html file', function() {
+describe('Restaurant on GET endpoint', function() {
 
-	it('should verify when you hit up root url', function() {
+	it('should return all existing restaurants', function() {
+		let res;
 		return chai.request(app)
-			.get('/')
+			.get('/restaurants')
+			.then(function(_res) {
+				res = _res;
+				res.should.have.status(200);
+				res.should.be.json;
+				res.body.should.be.a('array');
+				res.body.length.should.be.at.least(1);
+				res.body.forEach(function(restaurant) {
+					restaurant.should.be.a('object');
+				}); //.forEach function
+			}); //.then function
+	}); //it(should return all existing restaurants)
+
+	it('should return restaurants with right fields', function() {
+		let resRestaurant;
+		return chai.request(app)
+			.get('/restaurants')
 			.then(function(res) {
 				res.should.have.status(200);
-				res.should.be.html;
-			}); //.then
-	}); //it(should verify)
-}); //describe
+				res.should.be.json;
+				res.body.should.be.a('array');
+				res.body.length.should.be.at.least(1);
+
+				res.body.forEach(function(restaurant) {
+					restaurant.should.be.a('object');
+					restaurant.should.include.keys(
+						'id', 'name', 'cuisine', 'address', 'city', 'state');
+				}); //.forEach function
+			});
+	}); //it(should return restaurants with the right fields)
+}); //describe(Restaurant on GET point)
