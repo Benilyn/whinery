5ab0e8e124ca889e7e44f79f341fc7fea7590aa1
Benilyn <benilyn.gonzales@gmail.com>
2017-06-29 02:26:42 -0700

    create test for POST endpoint

--- test/test-server.js
+++ test/test-server.js
@@ -43,3 +43,31 @@ describe('Restaurant on GET endpoint', function() {
 			});
 	}); //it(should return restaurants with the right fields)
 }); //describe(Restaurant on GET point)
+
+describe('Restaurant on POST endpoint', function() {
+
+	it('should add a new restaurant', function() {
+		const newRestaurant = {
+			name: "Testing Restaurant Name",
+			cuisine: "American",
+			address: "123 Test Drive Ave.",
+			city: "Wala Wala",
+			state: "CA"
+		}; //const newRestaurant
+
+		return chai.request(app)
+			.post('/restaurants')
+			.send(newRestaurant)
+			.then(function(res) {
+				res.should.have.status(201);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.include.keys(
+					'id', 'name', 'cuisine', 'address', 'city', 'state');
+				res.body.id.should.not.be.null;
+				res.body.should.deep.equal(Object.assign(newRestaurant, {
+					id: res.body.id
+				})); //res.body.should.deep.equal
+			}); //.then function
+	}); //it(should add a new restaurant)
+}); //describe(Restaurant on POST endpoint)
