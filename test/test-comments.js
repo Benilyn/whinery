f2f3f913505a2078778a07ec6bd6fcb83e5a947a
Benilyn <benilyn.gonzales@gmail.com>
2017-07-06 00:32:06 -0700

    test POST, DELETE and PUT endpoint

--- test/test-comments.js
+++ test/test-comments.js
@@ -87,5 +87,73 @@ describe('Comments API resource', function() {
 	}); //describe(Comment on GET endpoint)
 
 
+	describe('Comment on POST endpoint', function() {
+		it('should add a new comment', function() {
+			const newComment = {
+				author: faker.name.firstName(),
+				review: faker.lorem.sentences(2)
+			}; //const newComment
+
+			return chai.request(app)
+				.post('/comments')
+				.send(newComment)
+				.then(function(res) {
+					res.should.have.status(201);
+					res.should.be.json;
+					res.body.should.be.a('object');
+					res.body.should.include.keys(
+						'id', 'author', 'review', 'created');
+					res.body.id.should.not.be.null;
+					res.body.created.should.not.be.null;
+					res.body.review.should.equal(newComment.review);
+					res.body.author.should.equal(newComment.author);
+				}); //.then function
+		}); //it(should add a new comment)
+	}); //describe(Comment on POST endpoint)
+
+
+	describe('Comment on DELETE endpoint', function() {
+		it('should delete comment on DELETE', function() {
+			return chai.request(app)
+				.get('/comments')
+				.then(function(res) {
+					return chai.request(app)
+					.delete(`/comments/${res.body[0].id}`);
+				}) //.then function .delete
+				.then(function(res) {
+					res.should.have.status(204);
+				}); //.then function status(204)
+		}); //it(should delete comment on delete)
+	}); //describe(Comment on DELETE endpoint)
+
+
+	describe('Restaurant on PUT endpoint', function() {
+		it('should update comment on PUT', function() {
+			const updateComment = {
+				author: faker.name.firstName(),
+				review: faker.lorem.sentences(2)
+			}; //const updateComment
+
+			return Comment
+				.findOne()
+				.exec()
+				.then(function(comment) {
+					updateComment.id = comment.id;
+					return chai.request(app)
+						.put(`/comments/${comment.id}`)
+						.send(updateComment);
+				})
+				.then(function(res) {
+					res.should.have.status(201);
+					return Comment.findById(updateComment.id).exec();
+				})
+				.then(function(comment) {
+					comment.author.should.equal(updateComment.author);
+					comment.review.should.equal(updateComment.review);
+				});
+		}); //it(should update comment on PUT)
+	}); //describe (Comment on PUT endpoint)
+
+
 
 }); //describe(Comments API resource)
