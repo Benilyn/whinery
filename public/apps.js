97877f17ff81007d1f097a9679af1234c2d0b512
Benilyn <benilyn.gonzales@gmail.com>
2017-07-12 13:44:00 -0700

    code click events for sign-up and log-in

--- public/apps.js
+++ public/apps.js
@@ -1,30 +1,36 @@
-$(document).ready(function() {
-	$.getJSON('/data')
-		.then(function(res){
-			res.forEach(displayRestaurant);
-		});
-
-}); //document.ready
-
-
-function displayRestaurant(restaurant, index) {
-/*	$('.restaurants').append(
-		'<a href="restaurantDetails.html">' +
-		'<div><p>' + restaurant.name + '</p></div>' +
-		'</a>');
-*/	console.log(restaurant);
-	$('.restaurants').append(
-		`<a href="/data/${index}">
-			<div>
-				<ul>
-					<li class='name'> ${restaurant.name} </li>
-					<span class='address'> ${restaurant.address}</span><br>
-						<span class='city'> ${restaurant.city}</span>
-						<span class='state'> ${restaurant.state}</span>
-						<span class='zip'> ${restaurant.zipcode}</span><br>
-				</ul>
-			</div>
-		</a>`);
+/*
+function showUI() {
+	$('#login-page').removeClass('hide');
+	$('#signup-page').removeClass('hide');
+	$('#sign-up').removeClass('hide');
 }
+*/
+
+$(document).ready(function() {
+	$('.sign-up').click(function() {
+		$('#signup-page').removeClass('hide');
+		$('#login-page').addClass('hide');
+		$('#sign-up').addClass('hide');
+	});
+
+	$('#login-page').submit(function(event) {
+		event.preventDefault();
+		$('#login-page').addClass('hide');
+		$('#sign-up').addClass('hide');
+		$('#search-result').removeClass('hide');
+		displayMap();
+	});
+});
 
 
+
+function displayMap() {
+
+	navigator.geolocation.getCurrentPosition(function(position) {
+    	var currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
+    	var map = new google.maps.Map(document.getElementById('map'), {
+	      center: currentLocation,
+	      zoom: 13
+	    }); //map
+	});
+}
