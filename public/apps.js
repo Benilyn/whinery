28f88dd8ead1322d432e2f2cd5c67b87c7015f4a
Benilyn <benilyn.gonzales@gmail.com>
2017-07-23 23:50:33 -0700

    added event handler for #login-page form submit to get login data value

--- public/apps.js
+++ public/apps.js
@@ -1,38 +1,32 @@
 
 $(document).ready(function() {
+	
 	$('#sign-up').click(function() {
 		$('.section').addClass('hide');
 		$('#signup-page').removeClass('hide');
-	});
-
-	$('#login-page form').submit(function(event) {
-		event.preventDefault();
-		$('.section').addClass('hide');
-		$('#search-result').removeClass('hide'); 
-		displayMap();
-	});
+	}); //$('#sign-up').click(function()
 
 	$('#restaurants li').click(function() {
 		$('.section').addClass('hide');
 		$('#restaurant-info').removeClass('hide');
-	});
+	}); //$('#restaurants li').click(function()
 
 	$('#write-whine').click(function() {
 		$('.section').addClass('hide');
 		$('#restaurant-whine').removeClass('hide');
-	});
+	}); //$('#write-whine').click(function()
 
 	$('#restaurant-whine form').submit(function(event) {
 		event.preventDefault();
 		$('.section').addClass('hide');
 		$('#latest-feeds').removeClass('hide');
-	});	
+	});	//$('#restaurant-whine form').submit(function(event)
 
 	$('#back-button').click(function(event) {
 		event.preventDefault();
 		$('.section').addClass('hide');
 		$('#restaurant-info').removeClass('hide');
-	});
+	}); //$('#back-button').click(function(event)
 
 	$('#signup-page form').submit(function(event) {
 		event.preventDefault();
@@ -42,7 +36,7 @@ $(document).ready(function() {
 			phone: $('#signup-page [name="phone-number"]').val(),
 			email: $('#signup-page [name="email-address"]').val(),
 			password: $('#signup-page [name="password"]').val()
-		};
+		}; //const userData
 		$.ajax('/users', {
 			contentType: 'application/json',
 			data: JSON.stringify(userData),
@@ -50,21 +44,39 @@ $(document).ready(function() {
 		.then(function(res) {
 			$('.section').addClass('hide');
 			$('#login-page').removeClass('hide');
-		});
-	});
-});
+		}); //.then function
+	}); //$('#signup-page form').submit(function(event)
 
+	$('#login-page form').submit(function(event) {
+		event.preventDefault();
+		const loginData = {
+			email: $('#login-page [name="email-address"]').val(),
+			password: $('#login-page [name="password"]').val()
+		}; //const loginData
+		$.ajax('/users', {
+			contentType: 'application/json',
+			data: JSON.stringify(loginData),
+			type: 'POST'}) 
+		.then(function(res) {
+			$('.section').addClass('hide');
+			$('#search-result').removeClass('hide'); 
+			displayMap();
+		});	//.then function
+	}); //$('#login-page form').submit(function(event)
+
+
+}); //$(document).ready(function()
 
 
 
 function displayMap() {
-
 	navigator.geolocation.getCurrentPosition(function(position) {
     	var currentLocation = {lat: position.coords.latitude, lng: position.coords.longitude};
     	var map = new google.maps.Map(document.getElementById('map'), {
 	      center: currentLocation,
 	      zoom: 13
 	    }); //map
-	});
-}
+	}); //getCurrentPosition function
+} //function displayMap()
+
 
