00aec806c76557abc4836987ba3d4f179d9c7ffe
Benilyn <benilyn.gonzales@gmail.com>
2017-07-25 01:28:56 -0700

    removed const requiredFields "author" on POST endpoint

--- router/whinesRouter.js
+++ router/whinesRouter.js
@@ -6,7 +6,7 @@ const {PORT, DATABASE_URL} = require('../config');
 
 router.post('/', (req, res) => {
 	const requiredFields = [
-		'author', 'food', 'service', 'cleanliness', 'price'];
+		'food', 'service', 'cleanliness', 'price'];
 	for (let i=0; i<requiredFields.length; i++) {
 		const field = requiredFields[i];
 		if (!(field in req.body)) {
