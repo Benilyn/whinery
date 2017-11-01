fcd5adf76ce6b9291dc1b186b06ab8c59b51d03b
Benilyn <benilyn.gonzales@gmail.com>
2017-11-01 00:43:20 -0700

    add if statement for demo user on router.post

--- router/whinesRouter.js
+++ router/whinesRouter.js
@@ -10,8 +10,13 @@ router.post('/', (req, res) => {
 		console.error(message);
 	return res.status(401).send(message);
 	}
-
-
+	
+//demo
+	if (req.user.name = 'demo') {
+		const message = 'You must sign up and login to write whine';
+		console.error(message);
+	return res.status(401).send(message);
+	}
 
 	const requiredFields = [
 		'food', 'service', 'cleanliness', 'price', 'restaurant'];
